06b2e59c4722b5fbb6c0537e91f64bde0bbb50e4
I am Price <pricep@nevada.unr.edu>
2018-11-08 21:27:23 -0800

    Dear Dr. Harris, I finished documentation to the ASTNode. Love, Price

--- bgp/input/ast1.c
+++ bgp/input/ast1.c
@@ -1,7 +1,4 @@
 float main()
 {
-	float b[1][2];
-	char w[1][2][3][4];
-	2 + 3 + 3 + 'a' + 2.3;
-
+    int a;
 }
--- bgp/src/ASTNode.h
+++ bgp/src/ASTNode.h
@@ -195,6 +195,10 @@ protected:
         //friend ostream& operator<< (ostream &os, const ASTNode& output);
 };
 
+/*!
+ * @name ASTMathNode
+ * @brief class to hold all the math operation nodes
+ */
 class ASTMathNode : public ASTNode
 {
     public:
@@ -274,6 +278,11 @@ class ASTMathNode : public ASTNode
         string value;
 };
 
+
+/*!
+ * @name ASTAssignNode
+ * @brief node to hold all assignment operations
+ */
 class ASTAssignNode : public ASTNode
 {
     public:
@@ -353,6 +362,10 @@ class ASTAssignNode : public ASTNode
         string value;
 };
 
+/*!
+ * @name ASTVariableNode
+ * @brief node to hold variables in
+ */
 class ASTVariableNode : public ASTNode
 {
     public:
@@ -434,6 +447,10 @@ class ASTVariableNode : public ASTNode
         string printType();
 };
 
+/*!
+ * @name ASTConstNode
+ * @brief node to hold constant values
+ */
 class ASTConstNode : public ASTNode
 {
     public:
@@ -453,151 +470,450 @@ class ASTConstNode : public ASTNode
      */
     ASTConstNode(string node_label, int inputType, valueUnion value);
 
+    /*!
+     * @name getType
+     * @return returns the type of the node
+     */
     int getType() const;
+
+    /*!
+     * @name setType
+     * @param inputType
+     * @brief sets the type of the node to inputType
+     */
     void setType(int inputType);
 
+    /*!
+     * @name getValue
+     * @return returns a union holding the value of the node.
+     */
     valueUnion getValue() const;
+
+    /*!
+     * @name setValue
+     * @param inputValue
+     * @brief sets the value of the node equal to inputValue
+     */
     void setValue(valueUnion inputValue);
 
 
+    /*!
+     * @name printNode
+     * @param treeOutFile
+     * @brief prints out the tree formatted for a dot file
+     */
     void printNode(ostream &treeOutFile = cout) override;
 
     private:
+    /*!
+     * an int corresponding to the type of the node.
+     */
     int type;
+
+    /*!
+     * valueUnion holding the value of the node
+     */
     valueUnion value;
+
+    /*!
+     * @name printType
+     * @return returns the type of the node as a string
+     */
     string printType();
 };
 
+/*!
+ * @name ASTSelectionNode
+ * @brief node to hold selection statements
+ */
 class ASTSelectionNode : public ASTNode
 {
     public:
+        /*!
+         * @name ASTSelectionNode
+         * @param node_label
+         * @brief Creates a new object and sets label equal to node_label
+         */
         ASTSelectionNode(string node_label);
+
+        /*!
+         * @name walk
+         * @return Walks the AST. Returns true if it succeeds and false if it does now
+         */
         bool walk() const;
+
+        /*!
+         * @name printNode
+         * @param treeOutFile
+         * @brief prints the node output to a tree dot file
+         */
         void printNode(ostream &treeOutFile = cout) override;
 };
 
+/*!
+ * @name ASTIterationNode
+ * @brief Class to hold all the loop operations
+ */
 class ASTIterationNode : public ASTNode
 {
     public:
+        /*!
+         * @name ASTIterationNode
+         * @param node_label
+         * @brief creates a new object and sets label equal to node_label
+         */
         ASTIterationNode(string node_label);
+
+        /*!
+         * @name walk
+         * @return returns true of false based on if the tree was successfully walked
+         */
         bool walk() const;
+
+        /*!
+         * @name printNode
+         * @param treeOutFile
+         * @brief prints the node formatted for a dot tree
+         */
         void printNode(ostream &treeOutFile = cout) override;
 };
 
+
+/*!
+ * @name ASTIdNode
+ * @brief node to hold all the identifiers and their types
+ */
 class ASTIdNode : public ASTNode
 {
     public:
+        /*!
+         * @name ASTIdNode
+         * @param node_label
+         * @brief creates a node and sets label equal to node_label
+         */
         ASTIdNode(string node_label);
+
+        /*!
+         * @name ASTIdNode
+         * @param node_label
+         * @param inputId
+         * @brief Creates a new node object. Sets label equal to node_label and id equal to inputId
+         */
         ASTIdNode(string node_label, string inputId);
+
+        /*!
+         * @name walk
+         * @return returns true or false based on if the tree was successfully walked ot not.
+         */
         bool walk() const{}
+
+        /*!
+         * @name printNode
+         * @param treeOutFile
+         * @brief prints the node outputted for a tree dot file
+         */
         void printNode(ostream &treeOutFile = cout) override;
+
+        /*!
+         * @name getId
+         * @return returns the id of the node
+         */
         string getId() const;
+
+        /*!
+         * @name setId
+         * @param inputId
+         * @brief sets the id of the node to inputId
+         */
         void setId(string inputId);
+
+        /*!
+         * @name getType
+         * @return returns the type of the node
+         */
         int getType() const;
+
+        /*!
+         * @name setType
+         * @param inputType
+         * @brief sets the type of the node equal to inputType
+         */
         void setType( int inputType );
 
     private:
+        /*!
+         * @name printType
+         * @return returns a string the corresponds to the type of the node
+         */
         string printType() const; // I can only imagine you meant to do this. - Matt
+
+        /*!
+         * a sting the holds the id of the ndoe
+         */
         string id;
+
+        /*!
+         * an int that stores the type of the node
+         */
         int type;
 };
 
+
+/*!
+ * @name ASTTypeNode
+ * @brief Class to hold all the type nodes
+ */
 class ASTTypeNode : public  ASTNode
 {
     public:
+        /*!
+         * @name ASTTypeNode
+         * @param node_label
+         * @brief creates an object and sets the label equal to node_label
+         */
         ASTTypeNode(string node_label);
+
+        /*!
+         * @name ASTTypeNode
+         * @param node_label
+         * @param inputType
+         * @brief creates an object. Sets the label equal to node_label and the type equal to inputType
+         */
         ASTTypeNode(string node_label, int inputType);
+
+        /*!
+         * @name walk
+         * @return returns a bool corresponding to whether the tree was successfully walked or not.
+         */
         bool walk() const{}
+
+        /*!
+         * @name printNode
+         * @param treeOutFile
+         * @brief prints the node to a formatted tree dot file
+         */
         void printNode(ostream &treeOutFile = cout) override;
+
+        /*!
+         * @name getType
+         * @return returns the type of the node
+         */
         int getType() const;
+
+        /*!
+         * @name setType
+         * @param inputType
+         * @brief sets the type of the node to inputType
+         */
         void setType( int inputType );
     private:
+
+        /*!
+         * @name printType
+         * @return returns a string corresponding to the type of the node
+         */
         string printType() const;
+
+        /*!
+         * an int that holds the type of the node
+         */
         int type;
 };
 
 class ASTCastNode : public ASTNode
 {
     public:
+        /*!
+         * @name ASTCastNode
+         * @param node_label
+         * @brief creates an object and sets label equal to node_label
+         */
         ASTCastNode(string node_label);
+
+        /*!
+         * @name ASTCastNode
+         * @param node_label
+         * @param childNode
+         * @param castType
+         * @brief creates an object. Sets label to node_label, adds child node childNode, and type to castType
+         */
         ASTCastNode(string node_label, ASTNode* childNode, int castType);
+
+        /*!
+         * @name printNode
+         * @param treeOutFile
+         * @brief prints the node formatted for a tree dot file
+         */
         void printNode(ostream &treeOutFile  = cout);
+
+        /*!
+         * @name getType
+         * @return returns the type of the function
+         */
         int getType() const;
+
+        /*!
+         * @name setType
+         * @param inputType
+         * @brief sets the type of the node to inputType
+         */
         void setType( int inputType );
     private:
+        /*!
+         * @name printType
+         * @return returns a string corresponding to the type of the node.
+         */
         string printType() const;
+
+        /*!
+         * sets the type of the node
+         */
         int type;
 };
 
+/*!
+ * @name ASTArrayNode
+ * @brief Class to hold all the array nodes
+ */
 class ASTArrayNode : public ASTNode
 {
 public:
+    /*!
+     * @name ASTArrayNode
+     * @param node_label
+     * @param id
+     * @param typeSet
+     * @brief creates an object. sets label to node_label, identifier to id, and type to typeSet
+     */
 	ASTArrayNode(string node_label, string id, int typeSet);
+
+	/*!
+	 * @name ASTArrayNode
+	 * @param node_label
+	 * @param inputNode
+	 * @brief creates an object. sets label to node_label, adds all children from inputNode
+	 */
     ASTArrayNode(string node_label, ASTNode* inputNode);
+
+    /*!
+     * @name printNode
+     * @param treeOutFile
+     * @brief prints the node formatted for a tree dot file
+     */
 	void printNode(ostream &treeOutFile = cout) override;
+
+	/*!
+	 * @name setType
+	 * @param inputType
+	 * @brief sets the type of the node to inputType
+	 */
 	void setType( int inputType );
+
+	/*!
+	 * @name getType
+	 * @return the type of the node
+	 */
 	int getType() const;
+
+	/*!
+	 * @name getId
+	 * @return returns the identifier of the node
+	 */
 	string getId() const;
+
+	/*!
+	 * @name setId
+	 * @param inputId
+	 * @brief sets identifier to inputId
+	 */
 	void setId(string inputId);
+
+	/*!
+	 * @name getDimensions
+	 * @return returns the list of dimensions of the node
+	 */
 	list<int> getDimensions() const;
+
+	/*!
+	 * @name addDimension
+	 * @param inputDim
+	 * @brief adds a dimension to the node
+	 */
 	void addDimension(int inputDim);
+
+	/*!
+	 * @name addDimensions
+	 * @param inputDimensions
+	 * @brief adds all the dimensions in inputDimensions to the node
+	 */
 	void addDimensions(list<int> inputDimensions);
 
 private:
+    /*!
+     * list of ints of the dimensions of the array
+     */
 	list<int> dimensions;
+
+	/*!
+	 * string to hold the identifier of the array
+	 */
 	string identifier;
+
+	/*!
+	 * int to hold the type of the array
+	 */
 	int type;
 };
 
+/*!
+ * @name ASTDeclarationNode
+ * @brief class to hold the declaration node
+ */
 class ASTDeclarationNode : public ASTNode
 {
 	public:
+        /*!
+         * @name ASTDeclarationNode
+         * @param node_label
+         * @param inputType
+         * @param childNode
+         * @brief creates an object. sets label to node_label, type to inputType, and adds children from childNode.
+         * Will propegate the type to all children of the node.
+         */
 		ASTDeclarationNode(string node_label, int inputType, ASTNode* childNode);
+
+		/*!
+		 * @name printNode
+		 * @param treeOutFile
+		 * @brief prints the node formatted for a tree dot file
+		 */
 		void printNode(ostream &treeOutFile = cout) override;
+
+		/*!
+		 * @name setType
+		 * @param inputType
+		 * @brief sets the type of the node.
+		 */
 		void setType( int inputType );
+
+		/*!
+		 * @name getType
+		 * @return returns the type of the node
+		 */
 		int getType() const;
 	private:
+        /*!
+         * int to hold the type of the node in
+         */
 		int type;
+
+		/*!
+		 * @name constructorTypeSet
+		 * @param node
+		 * @param inputType
+		 * @brief recursively sets node and all node's children's type to inputType.
+		 */
 		void constructorTypeSet( ASTNode* node, int inputType );
 };
-
-
-
-/* Will, what the hell are you doing? - Matt
- * If you write function definitions in header files,
- * the program won't compile because of multiple definitions
- * caused by including this header file in multiple other files.
-string printType(int type)
-{
-	switch (type)
-	{
-		case Void:
-			return "void";
-		case Char:
-			return "char";
-		case Short:
-			return "short";
-		case Int:
-			return "int";
-		case Long:
-			return "long";
-		case Float:
-			return "float";
-		case Double:
-			return "double";
-		case Struct:
-			return "struct";
-		default:
-			return "No type";
-	}
-}
-*/
-/*class ASTSwitchNode : public ASTNode
-{
-    public:
-        ASTSwitchNode();
-        ASTSwitchNode(int node_label, int line_num, list<ASTNode*> child_list);
-        bool walk();
-};*/
 
 #endif //PROJECT_ASTNODE_H
