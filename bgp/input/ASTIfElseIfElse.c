16365d31c10e2d0a6f7a16e526fd34089d330ce1
matthewbeck1998 <matthewbeck@nevada.unr.edu>
2018-11-30 04:40:21 -0800

    Dear Dr. Harris, Added more test cases. Love, Matt

---
+++ bgp/input/ASTIfElseIfElse.c
@@ -0,0 +1,19 @@
+int main()
+{
+    int x, y;
+    x = 1;
+    y = 2;
+
+    if (x < y)
+    {
+        x = y + 1;
+    }
+    else if (x == y)
+    {
+        x = y + 2;
+    }
+    else
+    {
+        y = x + 3;
+    }
+}
