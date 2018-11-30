70c5f043b331fe56089d0c8c5c7ad16cd53c4b68
I am Price <pricep@nevada.unr.edu>
2018-11-29 22:23:35 -0800

    Dear Dr. Harris, I made test cases. Love, Price

---
+++ bgp/input/OffsetTestWithFunctionCalls.c
@@ -0,0 +1,22 @@
+
+float func1(int x, char y)
+{
+
+}
+void nothing()
+{
+
+}
+int func2(int x[5], float y, char z[5])
+{}
+int main()
+{
+    int x[2][3];
+    int y;
+    char help;
+    float aaf;
+    func1(y, help);
+    nothing();
+    func2(y, aaf, help);
+    //nope();
+}
