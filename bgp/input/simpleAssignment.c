c6778ccae6a03f0fe7c45d58ffca53db4e1753fc
William Garner <william.garner7@gmail.com>
2018-11-29 22:57:41 -0800

    Dear Dr. Harris, I created test cases. Love Will

---
+++ bgp/input/simpleAssignment.c
@@ -0,0 +1,21 @@
+int main()
+{
+	int LHS1;
+	int LHS2;
+	int LHS3;
+	int a;
+	int b;
+	int v[6];
+	int w[7];
+	int x[8];
+	int y[9];
+	int z[10];
+
+	LHS1 = a;
+	LHS2 = 100;
+	LHS3 = z[7];
+	y[8] = b;
+	x[5] = 101;
+	w[3] = v[2];
+}
+
