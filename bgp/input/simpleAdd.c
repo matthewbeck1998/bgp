814421d942040d1a7950ece36488ca17a78e36f6
I am Price <pricep@nevada.unr.edu>
2018-12-17 21:16:55 -0800

    Dear Dr. Harris, I finished the offsets again again again again again! Love, Price

--- bgp/input/simpleAdd.c
+++ bgp/input/simpleAdd.c
@@ -1,11 +1,8 @@
 int main()
 {
-	int h, k;
-	int help, me;
-	int what;
+	//int h, k;
+	//h = k;
 	int x = 1, y = 2, z = 3;
-	int w[5];
-	h = k;
 	z = x + z + 3 - 3 * 5 / z;
-    return 0;
+  return 0;
 }
