569aa4bf717c80e136daebcc34ec139bca728e8b
matthewbeck1998 <matthewbeck@nevada.unr.edu>
2018-12-17 23:34:47 -0800

    Dear Dr. Harris, Hopefully this output is correct. Love, Matt

--- bgp/output/simpleAdd.asm
+++ bgp/output/simpleAdd.asm
@@ -1,25 +1,26 @@
-func	main, 16
-addiu	$t0, $sp, FS
-subiu	$t1, $t0, getOffset(x)
-li	$t0, 1
-sw	$t0, 0($t1)
-addiu	$t0, $sp, FS
-subiu	$t1, $t0, getOffset(y)
-li	$t0, 2
-sw	$t0, 0($t1)
-addiu	$t0, $sp, FS
-subiu	$t1, $t0, getOffset(z)
-li	$t0, 3
-sw	$t0, 0($t1)
-addiu	$t0, $sp, FS
-subiu	$t1, $t0, getOffset(x)
-addiu	$t0, $sp, FS
-subiu	$t2, $t0, getOffset(z)
-lw	$t0, 0($t1)
-lw	$t1, 0($t2)
-add	$t2, $t0, $t1
-addiu	$t0, $sp, FS
-subiu	$t1, $t0, getOffset(z)
-sw	$t2, 0($t1)
-li	$t0, 0
-ret	$t18
+addiu	$t0, $sp, 8
+li	$t1, 1
+sw	$t1, 0($t1)
+addiu	$t1, $sp, 4
+li	$t2, 2
+sw	$t2, 0($t2)
+addiu	$t2, $sp, 0
+li	$t3, 3
+sw	$t3, 0($t3)
+addiu	$t3, $sp, 8
+addiu	$t4, $sp, 0
+lw	$t5, 0($t3)
+lw	$t3, 0($t4)
+add	$t4, $t5, $t3
+li	$t3, 3
+add	$t5, $t4, $t3
+li	$t3, 3
+li	$t4, 5
+mul	$t6, $t3, $t4
+addiu	$t3, $sp, 0
+lw	$t4, 0($t3)
+div	$t6, $t4
+mflo	$t3
+sub	$t4, $t5, $t3
+addiu	$t3, $sp, 0
+sw	$t4, 0($t3)
