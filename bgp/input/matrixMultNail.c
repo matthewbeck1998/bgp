958fd3048a20d988fe28a89ac2ef3394d6151df2
matthewbeck1998 <matthewbeck@nevada.unr.edu>
2018-12-18 19:00:12 -0800

    Dear Dr. Harris, Input was wrong. Hopefully this is better. Love, Matt

--- bgp/input/matrixMultNail.c
+++ bgp/input/matrixMultNail.c
@@ -10,30 +10,30 @@ int main()
     int C[3][3];
     int i, j, k;
 
-    for (i = 0; i < 3; i++)
+    for (i = 0; i < 3; ++i)
     {
-        for (j = 0; j < 3; j++)
+        for (j = 0; j < 3; ++j)
         {
             A[i][j] = i + j;
             printInt(A[i][j]);
         }
     }
 
-    for (i = 0; i < 3; i++)
+    for (i = 0; i < 3; ++i)
     {
-        for (j = 0; j < 3; j++)
+        for (j = 0; j < 3; ++j)
         {
             B[i][j] = A[i][j];
             printInt(B[i][j]);
         }
     }
 
-    for (i = 0; i < 3; i++)
+    for (i = 0; i < 3; ++i)
     {
-        for (j = 0; j < 3; j++)
+        for (j = 0; j < 3; ++j)
         {
             C[i][j] = 0;
-            for (k = 0; k < 3; k++)
+            for (k = 0; k < 3; ++k)
             {
                 C[i][j] = C[i][j] + A[i][k] * B[k][j];
             }
