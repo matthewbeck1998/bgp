fbb532b769b256a9b263856ee05d432b20ca75db
matthewbeck1998 <matthewbeck@nevada.unr.edu>
2018-12-18 17:06:27 -0800

    Dear Dr. Harris, These are test cases for printInt. Love, Matt

---
+++ bgp/input/printInt.c
@@ -0,0 +1,14 @@
+void printInt(int i)
+{
+   i = 1; 
+}
+
+
+int main()
+{
+    int i = 1;
+    printInt(i);
+    return 0;
+}
+
+
---
+++ bgp/output/printInt.asm
@@ -0,0 +1,37 @@
+start:	
+jal	main
+li	$v0, 10
+syscall	
+
+printInt:	
+subiu	$sp, $sp, 8
+sw	$ra, 4($sp)
+addiu   $t0, $sp, 0
+sw  $a0, 0($t0)
+addiu   $t0, $sp, 0
+lw  $a0, 0($t0)
+li  $v0, 1
+syscall
+
+$l0:	
+lw	$ra, 4($sp)
+addiu	$sp, $sp, 8
+jr	$ra
+
+main:	
+subiu	$sp, $sp, 8
+sw	$ra, 4($sp)
+addiu	$t0, $sp, 0
+li	$t1, 1
+sw	$t1, 0($t0)
+addiu   $t0, $sp, 0
+lw  $a0, 0($t0)
+jal printInt
+
+li	$t0, 0
+move	$v0, $t0
+j	$l1
+$l1:	
+lw	$ra, 4($sp)
+addiu	$sp, $sp, 8
+jr	$ra
